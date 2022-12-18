c807438120816abb3d05ca7fd76c3276628b161c
AntonioRNeto216 <antoniorolaneto216@gmail.com>
2022-12-17 23:55:29 -0300

    Nova Versao

--- main.py
+++ main.py
@@ -2,81 +2,118 @@ import os
 import cv2 as cv
 import matplotlib.pylab as plt
 import numpy as np
-from filename_script import change_filename
 
-def process_frame(image):
-    height = image.shape[0]
-    width = image.shape[1]
-
-    region_of_interest_vertices = [
-        (200, height),
-        (width/2, height/1.9),
-        (width/1.5, height)
-    ]
-    
-    canny_image = cv.Canny(image, 170, 200)
-    
-    cropped_image = region_of_interest(canny_image, np.array([region_of_interest_vertices], np.int32))
-    
-    lines = cv.HoughLinesP(
-        cropped_image,
-        rho = 4,
-        theta = np.pi/180,
-        threshold = 200,
-        lines = np.array([]),
-        minLineLength = 40,
-        maxLineGap = 25
-    )
-
-    return draw_the_lines(image, lines)
-    
-def main():
-    path = f'{os.getcwd()}/data/'
-    entries = change_filename(path)
-    
-    array_image = []
-    
-    for image in entries:
-        array_image.append(process_frame(cv.imread(f'{path}{image}')))
-    
-
-    for img in array_image:
-        plt.clf()
-        plt.imshow(img)
-        plt.pause(0.05)
-        # plt.draw()
-
-def draw_the_lines(img, lines):
-    img = np.copy(img)
-    blank = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv.line(blank, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
-    img = cv.addWeighted(img, 0.6, blank, 1, 0.0)
-    return img
-
-def region_of_interest(img, vertices):
+def region_of_interest(img, v):
     mask = np.zeros_like(img)
-    #channel_count = img.shape[2]
-    match_mask_color = (255, ) #* channel_count
-    cv.fillPoly(mask, vertices, match_mask_color)
-    masked_image = cv.bitwise_and(img, mask)
-    return masked_image
+    mask = cv.fillPoly(mask, v, (255, ))
+    masked_img = cv.bitwise_and(img, mask)
+    return masked_img
 
-    # img = cv.imread(f'{way}{entries[1]}')
-    # edges = cv.Canny(img, 75, 100)
 
-    # lines = cv.HoughLinesP(edges, 1, np.pi/180, 50, maxLineGap=50)
+def main():
+    path = f'{os.getcwd()}/data'
 
-    # for line in lines:
-    #     x1, y1, x2, y2 = line[0]
-    #     cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    for i in range(len(os.listdir(path))):
+        
+        # Lendo a imagem
+        img = cv.imread(f'{path}/{i}.png', cv.IMREAD_GRAYSCALE)
 
-    # cv.imshow('edges', edges)
-    # cv.imshow('image', img)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
+        # Obtendo height e width da imagem
+        height, width = img.shape[:2]
+
+        # Definindo ROI
+        region_of_interest_vertices = [
+            (200, height),
+            (width/2, height/1.9),
+            (width/1.5, height)
+        ]
+
+        # Aplicando Gaussian Blur (Para tirar ruídos indesejados e garantir que o algoritmo Canny não detecte nada além de bordas)
+        blur_img = cv.GaussianBlur(
+            src = img,
+            ksize = (5, 5),
+            sigmaX = 0
+        )
+
+        # Algoritmo para detecção de bordas
+        edges_img = cv.Canny(
+            image = blur_img,
+            threshold1 = 50,
+            threshold2 = 200
+        )
+
+        # ! Teste
+        # l = np.array([
+        #     [417, 373],
+        #     [556, 224],
+        #     [566, 220],
+        #     [433, 373]
+        # ], dtype=np.int32)
+
+        # Gera uma imagem apenas com os valores na ROI
+        cropped_img = region_of_interest(
+            edges_img, 
+            np.array([region_of_interest_vertices], np.int32)
+        )
+
+        # Obtendo as linhas usando Algoritmo de Hough
+        lines = cv.HoughLinesP(
+            image = cropped_img,
+            rho = 4,
+            theta = np.pi/180,
+            threshold = 100,
+            lines = np.array([]),
+            minLineLength = 50,
+            maxLineGap = 10
+        )
+    
+        # Criando uma matriz de Zeros com o mesmo tamanho da imagem original
+        line_image = np.zeros_like(img)
+
+        if lines is not None:
+            
+            line_groups = {}
+
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    line_angle = (y2 - y1)/(x2 - x1)
+
+                    if -0.4 <= line_angle <= 0.4:
+                        continue
+
+                    not_group_yet = True
+                    for angle in line_groups.keys():
+                        if angle - 0.5 <= line_angle <= angle + 0.5:
+                            line_groups[angle].append([x1, y1, x2, y2])
+                            not_group_yet = False
+                            break
+                        
+                    if len(line_groups) == 0 or not_group_yet:
+                        line_groups[line_angle] = [[x1, y1, x2, y2]]
+
+            for k, line_groups_val in line_groups.items():
+                first_point = [0, 0]
+                second_point = [0, 0]
+                for l in line_groups_val:
+                    first_point[0] += l[0]
+                    first_point[1] += l[1]
+                    second_point[0] += l[2]
+                    second_point[1] += l[3]
+                
+                first_point[0] = int(first_point[0] / len(line_groups_val))
+                first_point[1] = int(first_point[1] / len(line_groups_val))
+                second_point[0] = int(second_point[0] / len(line_groups_val))
+                second_point[1] = int(second_point[1] / len(line_groups_val))
+
+                line_image = cv.line(line_image, tuple(first_point), tuple(second_point),(255,0,0),5)
+
+            # Colocando as linhas na imagem original
+            lines_edges = cv.addWeighted(img, 0.8, line_image, 1, 1)
+
+            plt.clf()
+            plt.imshow(lines_edges)
+            plt.pause(0.05)
+
 
 if __name__ == '__main__':
     main()
